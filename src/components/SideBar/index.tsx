11aa853bf5933c70f9600b4461a0c2351e341ab5
Patrick Arruda <patrick@plugify.com.br>
2022-08-26 17:37:42 -0300

    fixing sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,29 +1,30 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SideBarItens from './SideBarItens';
 
 import { ReactComponent as EYLogo } from 'assets/ey_text.svg';
 import styles from './SideBar.module.scss';
 import { FaBars } from 'react-icons/fa';
-import classNames from 'classnames';
 
 export default function SideBar() {
 
-	const [isOpen, setIsOpen] = useState(false);
 	const menuRoutes = SideBarItens;
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.top_section}>
-				<FaBars />
+				<FaBars className={styles.bars} />
+				<EYLogo className={styles.logo} />
 			</div>
 			<ul className={styles.list}>
 				{
 					menuRoutes.map((routes, index) => (
-						<li key={index}>
-							<Link to={routes.path} className={styles.link}>
-								<div className={styles.icon}>{routes.icon}</div>
-							</Link>
+						<li key={index} className={styles.lista}>
+							<div className={styles.link_container}>
+								<Link to={routes.path} className={styles.link}>
+									<div className={styles.icon}>{routes.icon}</div>
+									<div className={styles.link_text}>{routes.name}</div>
+								</Link>
+							</div>
 						</li>
 					))
 				}
